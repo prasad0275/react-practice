244bc1cc7a1ca7600857fcfe8a0957be84fb5786
prasad0275 <sawant.prasad0275@gmail.com>
2023-10-15 13:28:08 +0530

    09themeSwitcher done

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // It is important to mention when we use dark mode
+  darkMode : "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
