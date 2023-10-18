ac6186b926db99af757849104e19adf29de4ff09
prasad0275 <sawant.prasad0275@gmail.com>
2023-10-18 12:31:17 +0530

    megaBlog pr started

---
+++ 12megaBlog/appwrite/auth.js
@@ -0,0 +1,73 @@
+import conf from "../conf/conf"
+import { Client, Account, ID } from "appwrite";
+// const client = new Client()
+//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//     .setProject('<PROJECT_ID>');               // Your project ID
+
+// const account = new Account(client);
+
+// const promise = account.create(ID.unique(), 'email@example.com', '');
+
+// promise.then(function (response) {
+//     console.log(response); // Success
+// }, function (error) {
+//     console.log(error); // Failure
+// });
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+
+        this.account = new Account(this.client)
+
+    }
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique, email, password, name)
+            if(userAccount){
+                //call another method   
+                return this.login({email, password})
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try{
+            return await this.account.createEmailSession(email, password)
+        }   
+        catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error :" , error)
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error :" , error)
+        }
+    }
+}
+
+const authService = new AuthService();
+export default authService
