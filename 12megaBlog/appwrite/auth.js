b13922f733c0f45fc9d05c9e1acf8584ccb335a5
prasad0275 <sawant.prasad0275@gmail.com>
2023-10-19 20:32:06 +0530

    next step in mega blog project. Config Service is written.

--- 12megaBlog/appwrite/auth.js
+++ 12megaBlog/appwrite/auth.js
@@ -1,18 +1,5 @@
 import conf from "../conf/conf"
 import { Client, Account, ID } from "appwrite";
-// const client = new Client()
-//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-//     .setProject('<PROJECT_ID>');               // Your project ID
-
-// const account = new Account(client);
-
-// const promise = account.create(ID.unique(), 'email@example.com', '');
-
-// promise.then(function (response) {
-//     console.log(response); // Success
-// }, function (error) {
-//     console.log(error); // Failure
-// });
 
 export class AuthService {
     client = new Client();
